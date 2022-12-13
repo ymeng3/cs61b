016060f58e82d4637352c474e051a75ed7022554
Justin Meng <justin.mengj@gmail.com>
2022-12-13 20:58:17 +1100

    lab3 finished

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FlikTest {
+
+    @Test
+    public void testFlik(){
+        assertEquals(Flik.isSameNumber(3, 3), true);
+    }
+
+
+}
